dde5015e1552bf8a50ce88a8614513c3d46e53a2
David Saltares <david.saltares@gmail.com>
2015-05-30 16:42:50 +0100

    Adds error message in case there are no conjugations Fixes problem with expander icons Tweaks layout

--- site/js/index.js
+++ site/js/index.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     $('select').material_select();
 
     $('#btnConjugate').click(function(){
@@ -25,14 +24,16 @@ $(document).ready(function(){
     function onConjugationSucceeded(data) {
         var ul = $('<ul class="collapsible" data-collapsible="accordion"></ul>')
 
+        if (!Array.isArray(data.conjugations) || data.conjugations.length === 0) {
+            onConjugationFailed();
+            return;
+        }
+
         data.conjugations.forEach(function(mode) {
             ul.append(createModeBlock(mode));
         });
 
-        var conjugationsContainer = $('#conjugations');
-        conjugationsContainer.empty();
-        ul.css('display', 'none');
-        ul.appendTo(conjugationsContainer).fadeIn('slow');
+        updateConjugationsContainer(ul);
 
         $('.collapsible').collapsible({
             accordion: false
@@ -41,25 +42,30 @@ $(document).ready(function(){
 
     function createModeBlock(mode) {
         var li = $('<li></li>');
-        var header = $('<div class="collapsible-header"><i class="mdi-navigation-expand-more"></i></div>')
+        var header = $('<div class="collapsible-header"><i class="mode-expander mdi-navigation-expand-more"></i></div>')
         var body = $('<div class="collapsible-body"></div>');
 
         header.append(mode.name);
 
         header.click(function () {
-            var moreClass = 'mdi-navigation-expand-more';
-            var lessClass = 'mdi-navigation-expand-less';
+            $('.mode-expander').each(function(index, expander){
+                expander = $(expander);
 
-            var i = $(this).find('i');
+                var currentName = expander.parent().text();
+                var moreClass = 'mdi-navigation-expand-more';
+                var lessClass = 'mdi-navigation-expand-less';
 
-            if (i.hasClass(moreClass)) {
-                i.removeClass(moreClass);
-                i.addClass(lessClass);
-            }
-            else {
-                i.removeClass(lessClass);
-                i.addClass(moreClass);
-            }
+                console.log("Current: ", currentName);
+
+                if (currentName !== mode.name || expander.hasClass(lessClass)) {
+                    expander.removeClass(lessClass);
+                    expander.addClass(moreClass);
+                }
+                else {
+                    expander.removeClass(moreClass);
+                    expander.addClass(lessClass);
+                }
+            });
         });
 
         li.append(header);
@@ -109,7 +115,34 @@ $(document).ready(function(){
     }
 
     function onConjugationFailed() {
-        console.log('Failed to conjugate');
+        var errorMsg = "Sorry, we couldn't find any conjugations for that" +
+            " verb. Make sure to use the infinitive.";
+
+        var errorBlock = $('<div class="col"></div>').append(
+            $('<div class="card-panel red darken-4"></div>').append(
+                $('<div class="row"></div>').append(
+                    $('<div class="col l2"></div>').append(
+                        $('<i class="medium mdi-alert-error"></i>')
+                    ),
+                    $('<div class="col l10"></div>').append(
+                        $('<div class="valign-wrapper"></div>').append(
+                            $('<p class="valign"></p>').append(
+                                $('<span class="white-text"></span>').append(errorMsg)
+                            )
+                        )
+                    )
+                )
+            )
+        );
+
+        updateConjugationsContainer(errorBlock);
+    }
+
+    function updateConjugationsContainer(element) {
+        var conjugationsContainer = $('#conjugations');
+        conjugationsContainer.empty();
+        element.css('display', 'none');
+        element.appendTo(conjugationsContainer).fadeIn('slow');
     }
 
 });
